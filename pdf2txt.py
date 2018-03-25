580c9f2c81203673005d5107dac3310fa9fcb5d6
Aryparikh <Aryparikh@users.noreply.github.com>
2018-03-25 16:32:52 +0530

    Update pdf2txt.py

--- pdf2txt.py
+++ pdf2txt.py
@@ -1,40 +1,7 @@
 """ Author - Aryamaan Parikh
 
 Python 2.7
-
-There are two kinds of pdf documents ; Scanned and Vectorized.
-Scanned documents are pictures, vectorized documents have text that you can select, in them.
-
-This program has a few functions.
-
-- validator() verifies if the doc is scanned or vectorized. It returns True if vectorized and false if scanned
-
-- scan2vec() converts scanned psfs to vectorized pdfs. It takes path as input
-and returns the path of the converted PDF.
-
-- vecTextExtractor() extracts text from a vectorized pdf. It returns a list of strings, where each element in this list corresponds to
-the text of each page in the pdf. This is very useful later on for further text processing.
-
-- convertWithPdfMiner() takes vectorized pdf as input, and returns a list of strings, as in vecTextExtractor, where each element
-corresponds to the text of each page in the pdf.
-
-"""
-
-"""
-The libraries used here are pypdfocr, PyPDF2 and pdfminer.
-
-
-- pypdfocr is used for converting scanned to vectorized pdfs.
-
-- PyPDF2 works well for English pdf text extraction.
-
-- pdfminer works well for non english pdf text extraction. (Note : It is REALLY slow.)
-
-* We had to work with documents of all languages (not just english) *
-
-This is really messy, I know, but for our POC, this worked out.
-Enterprise level solution could be Abbyy
-
+Look at pdfTextExtractor for documentation on this
 """
 import subprocess
 from pypdfocr.pypdfocr import PyPDFOCR
